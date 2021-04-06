7b0849c73fc85171346b6d564c059ea3bb29651d
a17jespe <a17jespe@student.his.se>
2021-04-06 09:16:43 +0200

    Bunch of stuff
    1. Adds button to main activity
    2. Adds fragment to second activity
    3. Adds webview to fragment
    4. Shows gif in webview

---
+++ app/src/main/java/com/example/screens/MA2_Fragment.java
@@ -0,0 +1,82 @@
+package com.example.screens;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MA2_Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+
+public class MA2_Fragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private WebView webview_gif;
+
+    public MA2_Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MA2_Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MA2_Fragment newInstance(String param1, String param2) {
+        MA2_Fragment fragment = new MA2_Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        WebView my_WebView = view.findViewById(R.id.webview_gif);
+        my_WebView.loadUrl("file:///android_asset/hello.gif"); // presents gif
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_m_a2_, container, false);
+
+
+
+    }
+}
--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button button = findViewById(R.id.MA1_button); // This line, in combination with the following listener, adds functionality to the button.
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
